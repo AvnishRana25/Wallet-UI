49add7d8aa53304035b2c73e887d4bc2e05906f2
AvnishRana25 <avnishrana797@gmail.com>
2025-05-16 22:50:32 +0530

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-import { Buffer } from "buffer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,13 +9,11 @@ export default defineConfig({
   define: {
     global: "globalThis",
     "process.env": JSON.stringify({}),
-    Buffer: "Buffer",
   },
   resolve: {
     alias: {
       stream: "rollup-plugin-node-polyfills/polyfills/stream",
       events: "rollup-plugin-node-polyfills/polyfills/events",
-      buffer: "buffer",
     },
   },
   optimizeDeps: {
